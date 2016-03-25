efaf088cfecb035a5283ae77c5d71f72cb89eb03
fangfei <fangfei@outlook.com>
2016-03-24 23:38:02 -0700

    Add files for leetcode

---
+++ LeetCodeEasy/LeeExecution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeEasy
+{
+    class ExecutionClass
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class LeetCodeEasySolution
+    {
+        public bool Leet292_CanWinNim(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+
+            // With less than 4 left, will always win the game
+            // for any # lager than 4, as long as it is not 4*i, will win
+            return n % 4 != 0;
+        }
+
+        public int Leet258_AddDigits(int num)
+        {
+            if (num<=0)
+            {
+                return num;
+            }
+
+            int rst = Leet258_helper(num);
+            while (rst > 9)
+            {
+                rst = Leet258_helper(rst);
+            }
+            return rst;
+        }
+
+        private int Leet258_helper(int n)
+        {
+            int rst = 0;
+            while (n > 0)
+            {
+                rst += n % 10;
+                n = n / 10;
+            }
+            return rst;
+        }
+    }
+}
---
+++ LeetCodeTesting/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeEasy;
+
+namespace LeetCodeEasy.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [TestMethod()]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+                    "Leet292TestData.CSV",
+                    "Leet292TestData#CSV",
+                    DataAccessMethod.Sequential)]
+        [DeploymentItem("Leet292TestData.CSV")]
+        public void Leet292_CanWinNimTest()
+        {
+            LeetCodeEasySolution leetEasy = new LeetCodeEasySolution();
+            int N = Int32.Parse(TestContext.DataRow[0].ToString());
+            bool expected = bool.Parse(TestContext.DataRow[1].ToString());
+            Assert.AreEqual(expected, leetEasy.Leet292_CanWinNim(N));
+        }
+
+        [TestMethod()]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+                    "Leet258TestData.CSV",
+                    "Leet258TestData#CSV",
+                    DataAccessMethod.Sequential)]
+        [DeploymentItem("Leet258TestData.CSV")]
+        public void Leet258_AddDigitsTest()
+        {
+            LeetCodeEasySolution leetEasy = new LeetCodeEasySolution();
+            int num = Int32.Parse(TestContext.DataRow[0].ToString());
+            int expected = Int32.Parse(TestContext.DataRow[1].ToString());
+            Assert.AreEqual(expected, leetEasy.Leet258_AddDigits(num));
+        }
+    }
+}
